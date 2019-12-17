b397ee04feba46440b862d56e80a506dfe0a4ac0
mak-aravind-lab <aravind.murugaiyan@gmail.com>
2019-12-17 11:57:07 +0530

    debug mode prompted to user

--- src/main/scala/org/mak/piecetour/app/Main.scala
+++ src/main/scala/org/mak/piecetour/app/Main.scala
@@ -7,41 +7,48 @@ import org.mak.piecetour.domain.model.piece.{MovementRule, Piece}
 import scala.collection.immutable.HashMap
 
 object Main extends App {
-
-  println("Type grid size : ")
+  println("Type grid size (Ex: 100 will be of 10 * 10): ")
   val userGridSize = scala.io.StdIn.readLine()
-
-  val chessBoard = ChessBoard(gridSize = userGridSize.toInt)
-  println("<DEBUG>Furnishing Chess Board before tour: ")
-  println(chessBoard.getPrintable(true))
-  println(chessBoard.getPrintable())
-  println("The size of the chessboard grid")
-  println("ROW SIZE:" + chessBoard.rowSize + " " + "COLUMN SIZE:" + chessBoard.columnSize)
-
+  val userGridSizeInt : Int = if (userGridSize != null)  userGridSize.toInt else 100
+  println("Run in Debug Mode (Y or N): ")
+  val debugMode = scala.io.StdIn.readLine()
+  val userDebugMode = if (debugMode != null && debugMode.equalsIgnoreCase("yes") || debugMode.equals("y")) true else false
+  val chessBoard = ChessBoard(gridSize = userGridSizeInt)
   val listOfLegalMoves = ("N", -3, 0) ::
-                         ("NW", -2,-2 ) ::
-                         ("W", 0, -3) ::
-                         ("SW", 2, -2) ::
-                         ("S", 3, 0 ) ::
-                         ("SE", 2, 2) ::
-                         ("E",0,3) ::
-                         ("NE", -2, 2) :: Nil
-
+    ("NW", -2,-2 ) ::
+    ("W", 0, -3) ::
+    ("SW", 2, -2) ::
+    ("S", 3, 0 ) ::
+    ("SE", 2, 2) ::
+    ("E",0,3) ::
+    ("NE", -2, 2) :: Nil
   val randomRowIndex = scala.util.Random.nextInt(chessBoard.rowSize)
   val randomColumnIndex = scala.util.Random.nextInt(chessBoard.columnSize)
   val startPosition = TilePosition(randomRowIndex, randomColumnIndex)
   val heuristicAlgorithm = new HeuristicAlgorithm(chessBoard)
   val positionsVisited: HashMap[Int, (Int, Int)] = HashMap[Int, (Int, Int)]()
   val chessman = Piece(currentPosition = startPosition,
-                          nextPosition = startPosition,
-                          MovementRule.buildRules(listOfLegalMoves),
-                          positionsVisited,
-                          heuristicAlgorithm
-                          )
-  println(chessman.toString)
+    nextPosition = startPosition,
+    MovementRule.buildRules(listOfLegalMoves),
+    positionsVisited,
+    heuristicAlgorithm
+  )
+
+  private def printDebugDetails(): Unit = {
+    println("<DEBUG>Furnishing Chess Board before tour: ")
+    println(chessBoard.getPrintable(true))
+    println("<DEBUG>Furnishing details of piece about to navigate: ")
+    println(chessman.toString)
+  }
+  if (userDebugMode) printDebugDetails()
+  println("Furnishing Chess Board before tour: ")
+  println(chessBoard.getPrintable())
+  println("The size of the chessboard grid")
+  println("ROW SIZE:" + chessBoard.rowSize + " " + "COLUMN SIZE:" + chessBoard.columnSize)
   chessman.startTour()
-  println("<DEBUG>Furnishing Chess Board after tour: ")
-  println(chessBoard.getPrintable(true))
+  if (userDebugMode) {
+    printDebugDetails()
+  }
   println("<RESULT>Furnishing Chess Board after tour: ")
   println(chessBoard.getPrintable())
   println("Number of Positions visited " + chessman.positionsVisited.size)
--- src/test/scala/org/mak/piecetour/algo/NavigationSpec.scala
+++ src/test/scala/org/mak/piecetour/algo/NavigationSpec.scala
@@ -14,7 +14,7 @@ class NavigationSpec extends FeatureSpec with GivenWhenThen with BeforeAndAfter 
   val chessBoard: ChessBoard = ChessBoard(100)
   val heuristicAlgorithm = new HeuristicAlgorithm(chessBoard)
   val positionsVisited: HashMap[Int, (Int, Int)] = HashMap[Int, (Int, Int)]()
-  val listOfLegalMoves: List[(String, Int, Int)]= ("N", -3, 0) ::
+  val listOfLegalMoves: List[(String, Int, Int)] = ("N", -3, 0) ::
     ("NW", -2, -2) ::
     ("W", 0, -3) ::
     ("SW", 2, -2) ::
@@ -26,7 +26,7 @@ class NavigationSpec extends FeatureSpec with GivenWhenThen with BeforeAndAfter 
   feature("A piece starting with a position 2,2 should have 6 possible moves") {
     scenario("get next possible move from position 2,2 by ranking the 6 possible moves") {
       Given("a board of grid size 100")
-      val mockNextPosition = TilePosition(3,8)
+      val mockNextPosition = TilePosition(3, 8)
       val currentPosition = TilePosition(3, 8)
       val piece = Piece(currentPosition = currentPosition,
         nextPosition = mockNextPosition,
@@ -34,9 +34,9 @@ class NavigationSpec extends FeatureSpec with GivenWhenThen with BeforeAndAfter 
         positionsVisited,
         heuristicAlgorithm)
       Then("the position next to move will be ranked as 2 provided the position already 2,2 visited")
-        piece.startTour()
-        assert(piece.currentPosition.visited == 100)
-        assert(piece.positionsVisited.size == 100)
+      piece.startTour()
+      assert(piece.currentPosition.visited == 100)
+      assert(piece.positionsVisited.size == 100)
     }
   }
 }
